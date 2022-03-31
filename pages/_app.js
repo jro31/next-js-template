e8e0460b7dd340138bc3bae19677275dbff7ebf6
Jethro Williams <jethrowilliams@outlook.com>
2022-03-31 14:43:56 +0100

    Remove fluff

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,9 @@
-import '../styles/globals.css'
+// TODO - Update favicon
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+import '../styles/globals.css';
 
-export default MyApp
+const MyApp = ({ Component, pageProps }) => {
+  return <Component {...pageProps} />;
+};
+
+export default MyApp;
